b93ede2bd74396f0f24783bcf4cd2f9c228d22ba
jasonLiu001 <wangliu@isoftstone.com>
2017-11-26 12:28:06 +0800

    added comments.

--- assets/js/github-comments.js
+++ assets/js/github-comments.js
@@ -2,14 +2,12 @@
 
 // use of ajax vs getJSON for headers use to get markdown (body vs body_html)
 
-function ParseLinkHeader(lnk)
-{
+function ParseLinkHeader(lnk) {
     var entries = lnk.split(",");
-    var links = { };
-    for (var i in entries)
-    {
+    var links = {};
+    for (var i in entries) {
         var entry = entries[i];
-        var link = { };
+        var link = {};
         link.name = entry.match(/rel="([^"]*)/)[1];
         link.url = entry.match(/<([^>]*)/)[1];
         link.page = entry.match(/page=(\d+).*$/)[1];
@@ -18,35 +16,54 @@ function ParseLinkHeader(lnk)
     return links;
 }
 
-function ShowComments(repo_name, comment_id, page_id)
-{
+/**
+ *
+ * @param commentUrl 评论地址
+ * @param btnClass 评论按钮样式
+ */
+function getBtnComment(commentUrl, btnText, formClass, btnClass) {
+    return "<form class='" + formClass + "' action='" + commentUrl + "' rel='nofollow'> <input class='" + btnClass + "' type='submit' value='" + btnText + "' /> </form>";
+}
+
+function ShowComments(repo_name, comment_id, page_id) {
     var api_comments_url = "https://api.github.com/repos/" + repo_name + "/issues/" + comment_id + "/comments" + "?page=" + page_id;
 
     $.ajax(api_comments_url, {
         headers: {Accept: "application/vnd.github.v3.html+json"},
         dataType: "json",
-        success: function(comments, textStatus, jqXHR) {
+        success: function (comments, textStatus, jqXHR) {
 
+            var commentUrl = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
             // Add post button to first page
-            if (page_id == 1)
-            {
-                var url = "https://github.com/" + repo_name + "/issues/" + comment_id + "#new_comment_field";
-                $("#gh-comments-list").append("<form action='" + url + "' rel='nofollow'> <input type='submit' value='Post a comment on Github' /> </form>");
+            if (page_id == 1) {
+                $("#gh-comments-list").append(getBtnComment(commentUrl, "说说你的想法", "", "btn btn-outline-primary btn-lg btn-block"));
             }
 
             // Individual comments
-            $.each(comments, function(i, comment) {
+            $.each(comments, function (i, comment) {
 
                 var date = new Date(comment.created_at);
 
-                var t = "<div id='gh-comment'>";
-                t += "<img src='" + comment.user.avatar_url + "' width='24px'>";
-                t += "<b><a href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
-                t += " posted at ";
-                t += "<em>" + date.toUTCString() + "</em>";
-                t += "<div id='gh-comment-hr'></div>";
-                t += comment.body_html;
-                t += "</div>";
+                var t =
+                    "<div class='row'>" +
+                    "<div class='col-12 py-2'>" +
+                    "<div id='gh-comment'>" +
+                    "<div class='card'>";
+
+                //评论框头部
+                t += "<div class='card-header'><img class='rounded-circle' src='" + comment.user.avatar_url + "' width='30px' height='30px;'>";
+                t += "<b><a class='badge badge-light' href='" + comment.user.html_url + "'>" + comment.user.login + "</a></b>";
+                t += " 发表于 <em>" + moment(date).format('YYYY-MM-DD HH:mm:ss') + "</em>";
+                t += getBtnComment(commentUrl, "来个评论", "d-inline-flex ml-2", "btn btn-outline-primary") + "</div>";
+
+                //评论内容
+                t += "<div class='card-body'><p class='card-text'>" + comment.body_html + "</p><div>";
+
+                t +=
+                    "</div>" +
+                    "</div>" +
+                    "</div>" +
+                    "</div>";
                 $("#gh-comments-list").append(t);
             });
 
@@ -54,22 +71,19 @@ function ShowComments(repo_name, comment_id, page_id)
             var linksResponse = jqXHR.getResponseHeader("Link");
             if (linksResponse) {
                 var links = ParseLinkHeader(jqXHR.getResponseHeader("Link"));
-                if ("next" in links)
-                {
-                    ShowComments(repo_name, comment_id, page_id+1);
+                if ("next" in links) {
+                    ShowComments(repo_name, comment_id, page_id + 1);
                 }
             }
         },
-        error: function() {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
+        error: function () {
+            $("#gh-comments-list").append("<p>加载评论失败...</p>");
         }
     });
 }
 
-function DoGithubComments(repo_name, comment_id)
-{
-    $(document).ready(function ()
-    {
+function DoGithubComments(repo_name, comment_id) {
+    $(document).ready(function () {
         ShowComments(repo_name, comment_id, 1);
     });
 }
